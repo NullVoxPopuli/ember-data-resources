81b3d6b764a5fa57a5890a2629d729ffd8aa0475
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-08-04 09:16:55 -0400

    fix(request): add waitForPromise to .retry

--- addon/-private/resources/request.ts
+++ addon/-private/resources/request.ts
@@ -3,7 +3,7 @@ import { assert } from '@ember/debug';
 import { isDestroyed, isDestroying } from '@ember/destroyable';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { waitFor } from '@ember/test-waiters';
+import { waitFor, waitForPromise } from '@ember/test-waiters';
 
 import { Resource } from 'ember-resources';
 
@@ -54,7 +54,7 @@ export class Request<Args> extends Resource<Args> {
   }
 
   @action async retry() {
-    return this.__WRAPPED_FUNCTION__();
+    return waitForPromise(this.__WRAPPED_FUNCTION__());
   }
 
   @action
