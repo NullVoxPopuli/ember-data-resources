1cdd079cfcaa10d079e67c7bc743d0eed16c4361
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-24 12:09:06 -0400

    Update try scenarios

--- test-embroider-app/config/ember-try.js
+++ test-embroider-app/config/ember-try.js
@@ -13,9 +13,9 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0',
+            'ember-source': '~3.28.11',
             'ember-data': '~3.28.13',
-            '@ember-data/store': '~3.28.0',
+            '@ember-data/store': '~3.28.3',
           },
         },
       },
@@ -23,9 +23,9 @@ module.exports = async function () {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
-            'ember-data': '~4.4.0',
-            '@ember-data/store': '~4.4.0',
+            'ember-source': '~4.4.4',
+            'ember-data': '~4.4.1',
+            '@ember-data/store': '~4.4.1',
           },
         },
       },
@@ -33,7 +33,7 @@ module.exports = async function () {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
+            'ember-source': '~4.8.4',
             'ember-data': '~4.8.8',
             '@ember-data/store': '~4.8.8',
             '@ember-data/adapter': '~4.8.8',
@@ -47,7 +47,7 @@ module.exports = async function () {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
-            'ember-source': '~4.12.0',
+            'ember-source': '~4.12.1',
             'ember-data': '~4.12.0',
             '@ember-data/store': '~4.12.0',
           },
